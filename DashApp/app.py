b0e8b3452322be9209964cc05a8a0301a2d3585b
Maciek Zaweracz <mz1794@york.ac.uk>
2025-05-07 10:50:40 +0100

    update database path for docker

--- DashApp/app.py
+++ DashApp/app.py
@@ -2,6 +2,9 @@ import dash
 from callbacks.gene_callbacks import register_callbacks
 from layouts.gene_dashboard_layout import gene_dashboard_layout
 import dash_bootstrap_components as dbc
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize Dash app with Bootstrap theme
 app = dash.Dash(
--- DashApp/db/connection.py
+++ DashApp/db/connection.py
@@ -1,8 +1,8 @@
 import sqlite3
 import os
 
-DATABASE_PATH = "./UrotheliomeData.db"
-#DATABASE_PATH = os.getenv('DATABASE_PATH') or "'../data/UrotheliomeData.db'"
+#DATABASE_PATH = "./UrotheliomeData.db"
+DATABASE_PATH = os.getenv('DATABASE_PATH') or "'../data/UrotheliomeData.db'"
 
 def get_db_connection() -> sqlite3.Connection:
     """Establish and return a connection to the SQLite database."""
